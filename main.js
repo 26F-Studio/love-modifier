48d848eda84d769530b8d9b0721cafb5b341aed2
ParticleG <particle_g@outlook.com>
2022-07-02 00:30:37 +0800

    - Update sources

--- main.js
+++ main.js
@@ -1,24 +1,18 @@
-const fs = require('fs')
-const xcode = require('xcode')
+const fs = require('fs');
+const xcode = require('xcode');
 
-if (process.argv.length !== 3) {
-    console.error("Please pass the development team ID as the first argument")
-    process.exit(1)
-}
-const developmentTeamId = process.argv[2]
-const path = 'ios/App/App.xcodeproj/project.pbxproj'
-const project = xcode.project(path)
+const developmentTeamId = process.argv[2];
+const path = './love/platform/xcode/love.xcodeproj/project.pbxproj';
+const project = xcode.project(path).parseSync();
 
-project.parse(error => {
-    const targetKey = project.findTargetKey('App')
-    const appGroupKey = project.findPBXGroupKey({path: 'App'})
-    project.addBuildProperty('CODE_SIGN_ENTITLEMENTS', 'App/App.entitlements')
-    project.addBuildProperty('DEVELOPMENT_TEAM', developmentTeamId)
-    project.addFile('App.entitlements', appGroupKey)
-    project.removeFile('GoogleService-Info.plist', appGroupKey)
-    const f = project.addFile('GoogleService-Info.plist', appGroupKey, {target: targetKey})
-    f.uuid = project.generateUuid()
-    project.addToPbxBuildFileSection(f)
-    project.addToPbxResourcesBuildPhase(f)
-    fs.writeFileSync(path, project.writeSync())
-})
+const targetKey = project.findTargetKey('love-macosx');
+const resourcesGroupKey = project.findPBXGroupKey({path: 'Resources'});
+// project.addBuildProperty('CODE_SIGN_ENTITLEMENTS', 'App/App.entitlements');
+// project.addBuildProperty('DEVELOPMENT_TEAM', developmentTeamId);
+// project.addFile('App.entitlements', resourcesGroupKey);
+// project.removeFile('GoogleService-Info.plist', resourcesGroupKey);
+const pbxFile = project.addFile('./target.love', resourcesGroupKey, {target: targetKey});
+pbxFile.uuid = project.generateUuid();
+project.addToPbxBuildFileSection(pbxFile);
+project.addToPbxResourcesBuildPhase(pbxFile);
+fs.writeFileSync(path, project.writeSync());
