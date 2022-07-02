72de88c3231b12a9c851f35e461ec1dadcc9f380
ParticleG <particle_g@outlook.com>
2022-07-03 00:54:31 +0800

    - Update sources

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
 const fs = require('fs');
+const plist = require('plist');
 const xcode = require('xcode');
 
 const pbxprojPath = './love/platform/xcode/love.xcodeproj/project.pbxproj';
-
 const project = xcode.project(pbxprojPath).parseSync();
-
-project.updateProductName('Quatrack');
+project.updateProductName('${{ inputs.appName }}');
 const resourcesGroupKey = project.findPBXGroupKey({name: 'Resources'});
-const targetKey = project.findTargetKey('"love-ios"')
+const targetKey = project.findTargetKey('"love-macosx"')
 project.addResourceFile('./target.love', {target: targetKey}, resourcesGroupKey);
-
 fs.writeFileSync(pbxprojPath, project.writeSync());
+
+const plistPath = './love/platform/xcode/macosx/love-macosx.plist';
+const parsed = plist['parse'](fs.readFileSync(plistPath, 'utf8'));
+parsed['CFBundleName'] = '${{ inputs.appName }}';
+fs.writeFileSync(plistPath, plist['build'](parsed));
 
 // project.addBuildProperty('CODE_SIGN_ENTITLEMENTS', 'App/App.entitlements');
 // project.addBuildProperty('DEVELOPMENT_TEAM', developmentTeamId);
--- plist.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const plist = require('plist');
-
-const plistPath = './love/platform/xcode/macosx/love-macosx.plist';
-
-const parsed = plist['parse'](fs.readFileSync(plistPath, 'utf8'));
-parsed['CFBundleName'] = 'Quatrack';
-
-fs.writeFileSync(plistPath, plist['build'](parsed));
