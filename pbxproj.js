801d1356d035cef65282b4b7337c6d0db40a034b
ParticleG <particle_g@outlook.com>
2022-07-02 21:55:50 +0800

    - Update sources

--- pbxproj.js
+++ pbxproj.js
@@ -4,14 +4,12 @@ const xcode = require('xcode');
 const pbxprojPath = './love/platform/xcode/love.xcodeproj/project.pbxproj';
 
 const project = xcode.project(pbxprojPath).parseSync();
-project.pbxTargetByName('love-macosx').productName = 'Quatrack';
-// const targetKey = project.findTargetKey('love-macosx');
-// const resourcesGroupKey = project.findPBXGroupKey({name: 'Resources'});
-// const pbxFile = project.addFile('./target.love', resourcesGroupKey, {target: targetKey});
-// pbxFile.uuid = project.generateUuid();
-// project.addToPbxBuildFileSection(pbxFile);
-// project.addToPbxResourcesBuildPhase(pbxFile);
-//
+
+project.updateProductName('Quatrack');
+const resourcesGroupKey = project.findPBXGroupKey({name: 'Resources'});
+const targetKey = project.findTargetKey('"love-ios"')
+project.addResourceFile('./target.love', {target: targetKey}, resourcesGroupKey);
+
 fs.writeFileSync(pbxprojPath, project.writeSync());
 
 // project.addBuildProperty('CODE_SIGN_ENTITLEMENTS', 'App/App.entitlements');
