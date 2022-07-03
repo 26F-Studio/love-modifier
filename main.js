2490bdeb85b0cb75967eca21e26602a2a084c2e3
ParticleG <particle_g@outlook.com>
2022-07-03 08:49:16 +0800

    - Update sources

--- main.js
+++ main.js
@@ -8,13 +8,24 @@ project.updateProductName('${{ inputs.appName }}');
 const resourcesGroupKey = project.findPBXGroupKey({name: 'Resources'});
 const targetKey = project.findTargetKey('"love-macosx"')
 project.addResourceFile('./target.love', {target: targetKey}, resourcesGroupKey);
+console.info('Project info updated.');
 fs.writeFileSync(pbxprojPath, project.writeSync());
 
 const plistPath = './love/platform/xcode/macosx/love-macosx.plist';
 const parsed = plist['parse'](fs.readFileSync(plistPath, 'utf8'));
 parsed['CFBundleName'] = '${{ inputs.appName }}';
+parsed['NSHumanReadableCopyright'] = 'Copyright © 2019-' + new Date().getFullYear() + ' 26F-Studio. Some Rights Reserved.';
+delete parsed['CFBundleDocumentTypes'];
 delete parsed['UTExportedTypeDeclarations'];
+console.info(parsed);
 fs.writeFileSync(plistPath, plist['build'](parsed));
+
+const iconPath = './love/platform/xcode/Images.xcassets/OS X AppIcon.appiconset/Contents.json';
+const iconContents = JSON.parse(fs.readFileSync(iconPath, 'utf8'));
+iconContents.images.forEach(image => {
+    image.filename = 'icon_' + image.size + image.scale === '2x' ? '@2x' : '' + '.png';
+});
+console.info(iconContents);
 
 // project.addBuildProperty('CODE_SIGN_ENTITLEMENTS', 'App/App.entitlements');
 // project.addBuildProperty('DEVELOPMENT_TEAM', developmentTeamId);
